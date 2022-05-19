dbe0bf663fdb9edaa7515c5c3875404a02b90c57
Hellosager <sebas.b@web.de>
2022-05-19 20:52:08 +0200

    init

---
+++ TypoMemer.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Diagnostics;
+
+namespace TypoMemer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void SetActiveWindow(IntPtr windowHandle) => SetForegroundWindow(windowHandle);
+
+        private const int HOTKEY_ID = 1337;
+
+        private const uint MOD_CONTROL = 0x0002; // CTRL
+        private const uint VK_SPACE = 0x20;
+
+        IntPtr handle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_CONTROL, VK_SPACE); //CTRL + Space
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_SPACE)
+                            {
+                                handle = GetForegroundWindow();
+                                this.WindowState = WindowState.Normal;
+                                this.Show();
+                               // const int nChars = 256;
+                               // StringBuilder Buff = new StringBuilder(nChars);
+                               // if (GetWindowText(handle, Buff, nChars) > 0)
+                               //     Debug.WriteLine("Coming from a " + Buff.ToString());
+
+                                Debug.Write("Space was pressed" + Environment.NewLine);
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+            base.OnClosed(e);
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (SetForegroundWindow(handle))
+                    System.Windows.Forms.SendKeys.SendWait("Hello World*");
+
+
+                Debug.WriteLine("Enter was pressed");
+            }
+
+        }
+    }
+}
